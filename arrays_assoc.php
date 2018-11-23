1d17863a136839fb8257b4ee943eafb4ee3a8a57
hemanshu.eng <hemanshu.eng@gmail.com>
2018-11-23 17:08:15 +0000

    add new file

---
+++ arrays_assoc.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$number = array('hemanshu','anita','krishana');
+echo $number[2]."<br>";
+print_r($number);
+echo '<br>';
+$names =  array("first_name"=>'edwin',"last_name"=>'diaz');
+echo $names['first_name']." ".$names['last_name']."<br>";
+print_r($names);
+?>
+</body>
+</html>
+
---
+++ switch.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+     $number = 5;
+     switch($number){
+            case 1:
+            echo "this is one";
+            break;
+            case 2:
+            echo "this is two";
+            break;
+            case 3: 
+            echo "this is three";
+            break;
+            case 4:
+            echo "this is four";
+            break;
+            default:
+            echo "there is no match";
+    
+     }
+    ?>
+</body>
+</html>
