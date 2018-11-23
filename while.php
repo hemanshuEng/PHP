c98b42e7db95af15f3b8fdf69c8a01f3b272de5f
hemanshu.eng <hemanshu.eng@gmail.com>
2018-11-23 17:40:56 +0000

    added new file for ,while ,foreach

---
+++ while.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>While</title>
+</head>
+<body>
+    <?php
+        $counter = 0;
+        while ($counter <10){
+            echo "hello student"."<br>";
+            $counter++;
+        }
+        
+        //while(4<10){
+        //    echo "hello student";
+        //}
+    ?>
+</body>
+</html>
